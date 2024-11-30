248429eb4fabaa903b013ac903c90eb588366e10
Jason Ernst <ernstjason1@gmail.com>
2024-11-30 14:48:15 -0800

    bring inline with ansible-lint

--- ansible/roles/rust_game/files/wipe-monthly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# run as root
-# https://www.bisecthosting.com/clients/index.php?rp=/knowledgebase/618/How-to-setup-automatic-server-wipes-on-a-Rust-server.html
-docker stop rust-monthly
-rm server/rust_docker/player.deaths.*
-rm server/rust_docker/*.map
-rm server/rust_docker/*.sav*
-sudo sed -i~ '/^RUST_SERVER_SEED=/s/=.*/='$RANDOM'/' seed.env
-docker start rust-monthly
